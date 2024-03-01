44fa2e94b41414d9e4394098653148985dd51dd2
chintan224 <161326876+chintan224@users.noreply.github.com>
2024-03-01 12:17:01 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Records from './components/Records.js'
+import Pagination from './components/Pagination.js' 
+
+
+
+
 function App() {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [recordsPerPage] = useState(10)
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res =>  {
+        setData(res.data)
+        setLoading(false);
+      })
+      .catch(() => {
+          alert('Error while loading data')
+      })
+  }, [])
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage
+  const currentRecords = data.slice(indexOfFirstRecord - indexOfLastRecord)
+  const nPages = Math.ceil(data.length / recordsPerPage)  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container mt-5'>
+        <h2> Pagination Example in React</h2>
+        <Records data={currentRecords} />
+        <Pagintaion
+          nPages={nPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+    </div>   
+  )
 }
 
 export default App;
